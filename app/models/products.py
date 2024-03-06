acc937a9e60bf410cf6e25506a1aae9059c21221
sdubey86 <65734795+sdubey86@users.noreply.github.com>
2024-03-06 19:55:21 +0530

    commiting database and model

--- app/models/products.py
+++ app/models/products.py
@@ -1,2 +1,13 @@
+from sqlalchemy import Integer, String, Float, Column, Text 
+from app.db.database import Base  # Import the Base class
 
+# Product SQLAlchemy Model
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(Text)
+    price = Column(Float, nullable=False)
+    quantity = Column(Integer, nullable=False)
 
